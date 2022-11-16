736a3c6301704501758c4171cbf8b376b7a4cee0
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-11-16 12:22:25 +0100

    updates

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const app = require('express')();
-
-const PORT = 8080;
-
-app.get('/tshirt',(req,res)=>{
-    res.status(200).send({
-        tshirt:'demo'
-    })
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const port = 5000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/image", express.static("image")); 
+let imageName = ""; 
+const storage = multer.diskStorage({
+  destination: path.join("./image"), 
+  filename: function (req, file, cb) {
+    imageName = Date.now() + path.extname(file.originalname);
+    cb(null, imageName);
+  },
 });
-
-app.listen(
-    PORT,
-    ()=> console.log(`it's a live on http://localhost:${PORT}`)
-    )
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 3000000 },
+}).single("myImage");
+app.post("/upload-image", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      return res.status(201)
+      .json({ url: "http://localhost:5000/image/" + imageName }); 34
+    }
+  });
+});
+app.listen(port, () => {
+  console.log("server run in port", port);
+});
